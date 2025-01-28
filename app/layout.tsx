ae6f45f1baac600ab9577486f3d06ab51f450be2
rimonppip0 <rimonppip0@gmail.com>
2025-01-28 06:21:53 +0000

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Image from 'next/image';
-
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "NU CSE CGPA Result Calculator",
@@ -26,11 +14,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body> */}
       <body className="bg-gradient-to-r from-blue-50 to-blue-100">
         <header className="flex items-center p-4 bg-white shadow-md">
           <Image src="/logo.png" alt="CGPA Calculator Logo" width={50} height={50} />
